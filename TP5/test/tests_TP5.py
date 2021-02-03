d10a1c816576b8e3104b7cbac34fdb51c5e6d691
Mégane <72190376+MeganeBoujeant@users.noreply.github.com>
2021-02-03 16:20:13 +0100

    modif TP5

---
+++ TP5/test/tests_TP5.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    Author : Mégane Boujeant
+
+    example of exception to understand :
+
+        def divide(a, b):
+            if b == 0:
+                raise Exception("On ne divise pas par 0")
+            return (a / b)
+
+        if __name__ == "__main__":
+            try:
+                divide(5, 0)
+            except Exception:
+                print("You have an error")
+"""
+
+from tree_node import TreeNode
+from tree_node_2 import Tree
+
+
+
